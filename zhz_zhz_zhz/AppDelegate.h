d8e2078097e2dadb73cad6c0adce447a28a185e4
ZhenHangZhang <zhanghangzhen@z.local>
2016-07-13 15:28:34 +0800

    Initial Commit

---
+++ zhz_zhz_zhz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhz_zhz_zhz
+//
+//  Created by zhanghangzhen on 16/7/13.
+//  Copyright © 2016年 zhanghangzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhz_zhz_zhz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhz_zhz_zhz
+//
+//  Created by zhanghangzhen on 16/7/13.
+//  Copyright © 2016年 zhanghangzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
